53e3568cefc61a36643f9c2cc3e82be3dee6ed2e
Branel <emirpod02@gmail.com>
2021-05-08 19:41:31 +0200

    Update

--- Scripts/lastUpdate.js
+++ Scripts/lastUpdate.js
@@ -1,4 +1,4 @@
 function dateFunction() {
     var x = new Date(document.lastModified);
-    document.getElementById("demo").innerHTML = x;
+    document.getElementById("lastUpdate").innerHTML = x;
 }
