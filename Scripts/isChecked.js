2796ec885807a438523e140da0fe2ac09773f470
Branel <emirpod02@gmail.com>
2021-05-10 10:30:09 +0200

    Update isChecked.js

--- Scripts/isChecked.js
+++ Scripts/isChecked.js
@@ -1,13 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.querySelector('input[type="checkbox"]');
-  
+
     checkbox.addEventListener('change', function () {
       if (checkbox.checked) {
         // do this
-        swapStyles('../CSS/Darkmode.css')
+        swapStyles('/CSS/Darkmode.css')
       } else {
         // do that
-        swapStyles('../CSS/Lightmode.css')
+        swapStyles('/CSS/Lightmode.css')
       }
     });
   });
+
+var setTheme = localStorage.getItem('theme')
+console.log('theme:', setTheme)
+if (setTheme == null){
+    swapStyle('../CSS/Lightmode.css')
+}else{
+    swapStyle(setTheme)
+  }
+  function swapStyle(sheet){
+      document.getElementById('mystylesheet').href = sheet
+      localStorage.setItem('theme', sheet)
+  }
+
+  $(document).ready(function(){
+    var setdark = function () {
+        $('#head').append('<link rel="stylesheet" href="CSS/dark.css" type="text/css" />');
+    },
+    setlight = function () {
+        $('link[rel=stylesheet][href="CSS/dark.css type="text/css"]').remove();
+    };
+    
+    $("#light").click(function () {
+        localStorage.setItem('color', 'light');
+        setlight();
+    });
+    
+    $("#dark").click(function () {
+        localStorage.setItem('color', 'dark');
+        setdark();
+    });
+    
+    if (localStorage.getItem('color') == 'light') {
+        setlight(); 
+    } 
+    
+    else if (localStorage.getItem('color') == 'dark') {
+        setdark();
+     }
+    })
